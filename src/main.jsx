2768898c8b95aa6270b2c971b8536eaeb543435d
Karan Kumar <karankannaujiya129@gmail.com>
2026-07-26 17:04:59 +0530

    Improve global keyboard focus states

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './App.css';
 import App from './App';
+import './styles/ui-polish.css';
 
 // OPTIONAL: one global reset/theme file only
 // import './styles/global.css';
